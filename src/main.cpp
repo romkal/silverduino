827b64f83f57bf5f620d2fe34180f24f3b9eae10
Roman Kalukiewicz <roman.kalukiewicz@gmail.com>
2017-01-28 15:25:06 -0800

    Now it should track better...

--- src/main.cpp
+++ src/main.cpp
@@ -5,51 +5,60 @@
  *      Author: romkal
  */
 
-#include "Arduino.h"
+#include <Arduino.h>
 
-const int MD1 = 11;
-const int KSL = 7;
-const int DOB = 6;
-const int CCP = 5; // Has to be 1 or 2
-const int HOK = 4;
+const int MD1 = 6;
+const int KSL = 5;
+const int DOB = 4;
+const int CCP = 3; // Has to be 1 or 2
+const int HOK = 2;
 const int BUTTON = 8;
 
-volatile int currentNeedle = 0;
-volatile int camLeft = 0;
-volatile int camRight = 0;
-volatile bool wasBetweenCams = false;
+const int MAX = 200;
 
+volatile int currentNeedle = 0;
+volatile int camLeft = -MAX * 2;
+volatile int camRight = MAX * 2;
+volatile bool wasBetweenCams = false;
+volatile bool wasGoingRight = true;
 volatile int row = 0;
 
+void setCams(bool goingRight) {
+	if (goingRight && camRight > MAX) {
+		camRight = currentNeedle;
+	}
+	if (!goingRight && camLeft < MAX) {
+		camLeft = currentNeedle;
+	}
+}
+
 void updatePosition() {
-	bool goingRight = digitalRead(HOK) == HIGH;
+	bool goingRight = digitalRead(HOK) == LOW;
 	bool insideCams = digitalRead(KSL) == HIGH;
 	bool needle1 = digitalRead(MD1) == LOW;
-	currentNeedle += (goingRight ? 1 : -1);
-	// we left cams
+	bool changedDirection = wasGoingRight != goingRight;
+	int direction = (goingRight ? 1 : -1);
+
+	// Left cams
 	if (!insideCams && wasBetweenCams) {
 		row++;
-		if (wasBetweenCams) {
-			// We went outside of cams
-			if (goingRight) {
-				camRight = currentNeedle;
-			} else {
-				camLeft = currentNeedle;
-
-			}
-
-		}
+		setCams(goingRight);
 	}
-	// we entered cams
-	if (insideCams && !wasBetweenCams) {
+	currentNeedle += direction;
+
+	// entered cams
+	if (!wasBetweenCams && insideCams)
+	{
+		setCams(goingRight);
 		currentNeedle = goingRight ? camLeft : camRight;
 	}
+
 	if (needle1) {
 		camLeft -= currentNeedle;
 		camRight -= currentNeedle;
-		currentNeedle = 0;
 	}
 	wasBetweenCams = insideCams;
+	wasGoingRight = goingRight;
 }
 
 void updateNeedle()
@@ -79,19 +88,18 @@ void setup()
 	  pinMode(BUTTON, INPUT_PULLUP);
 
 	  digitalWrite(DOB, LOW);
-	  attachInterrupt(digitalPinToInterrupt(CCP), onNeedle, FALLING);
+	  attachInterrupt(digitalPinToInterrupt(CCP), onNeedle, RISING);
 	  Serial.begin(9600);
 }
 
 void loop()
 {
 	if (digitalRead(BUTTON) == LOW) {
-		String text = String("current needle: ") + currentNeedle
+		String text = String("currurent needle: ") + currentNeedle
 				+ " cams: " + camLeft + ":" + camRight
 				+ " inside: " + wasBetweenCams
 				+ " row: " + row;
 		Serial.println(text);
-		row = 0;
 	}
-	delay(100);
+	delay(1000);
 }
